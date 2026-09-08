5e8e4b747d67ee5a10b25acb5fe1926cc409fd00
Wendy Whittle <jenmaydance@gmail.com>
2026-09-08 09:56:39 -0700

    Clean redundant generated research tests

--- tests/test_research_boundary_2.py
+++
@@ -1,6 +0,0 @@
-from app.research_endpoint import build_research_router
-
-
-def test_research_router_has_no_write_route():
-    router = build_research_router(["https://feeds.example.com/news"])
-    assert {method for route in router.routes for method in route.methods} == {"POST"}
